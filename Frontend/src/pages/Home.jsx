9c41c32106d5209052e289861be9ac63b19c920e
shreyash Jadhav <connectshreyashj96@gmail.com>
2025-08-11 11:38:00 +0530

    nav bug fix

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -5,7 +5,7 @@ import Nav from "../components/Navbar/Nav";
 function Home() {
     return (
         <div style={{ position: "relative", height: "100vh", overflow: "hidden" }} >
-            <video  autoPlay loop muted playsInline
+            <video autoPlay loop muted playsInline
                 style={{ position: "absolute", top: "50%", left: "50%", minWidth: "100%", minHeight: "100%", width: "auto", height: "auto", zIndex: "-1", transform: "translate(-50%, -50%)", objectFit: "cover", }}
             >
                 <source src={homePagevideo} type="video/mp4" />
@@ -16,7 +16,9 @@ function Home() {
 
             <div className="flex justify-center">
                 <h1 className="text-4xl font-bold text-white w-200 ">Transforming the Way You Look & Feel, <p className="text-center">While Empowering Skilled Professionals</p></h1>
+         
             </div>
+            <p></p>
         </div>
     );
 }
